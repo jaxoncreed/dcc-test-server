1936722440c5ecf83c9effcb22c6bc615f14b0b8
Jackson Morgan <jaxoncreed@gmail.com>
2024-03-21 10:23:57 -0400

    Extracts deep link

--- src/customFunctions.ts
+++ src/customFunctions.ts
@@ -1,4 +1,5 @@
 import type { ActionFn, Next, ScenarioContext, EventEmitter } from "artillery";
+import { URL } from "url";
 
 export const loadEnvironmentVariable: ActionFn = (
   context: ScenarioContext,
@@ -10,18 +11,25 @@ export const loadEnvironmentVariable: ActionFn = (
     if (!emailedUrlString)
       throw new Error("The emailed_url param must be provided");
     context.vars.emailedUrl = emailedUrlString;
+    const emailedUrl = new URL(emailedUrlString);
+    const token = emailedUrl.searchParams.get("token");
+    if (!token) throw new Error("The URL must have a token.");
+    context.vars.token = `Bearer ${token}`;
+    const dashboardLinkUrl = new URL(emailedUrl);
+    dashboardLinkUrl.pathname = "/api/get-credential-links";
+    dashboardLinkUrl.search = "";
+    context.vars.dashboardLinkUrl = dashboardLinkUrl.toString();
     next();
   } catch (e: unknown) {
     next(e instanceof Error ? e : new Error(`${e}`));
   }
 };
 
-export const processResult: ActionFn = (
+export const processDirectDeepLink: ActionFn = (
   context: ScenarioContext,
   _ee: EventEmitter,
   next: Next,
 ) => {
-  console.log(context.vars.plainTextResponse);
-  console.log("In here");
+  console.log("Direct Deep Link", context.vars.directDeepLink);
   next();
 };
